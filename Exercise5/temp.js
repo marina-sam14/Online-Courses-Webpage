d3213571a29981b222b840783b322b7ea2e819b0
marina-sam14 <marina-samb14@gmail.com>
2021-12-07 01:40:34 +0200

    Recap in all Exercises

--- Exercise5/temp.js
+++ Exercise5/temp.js
@@ -1,94 +1,74 @@
-// var form = document.getElementById("register")
+const form = document.getElementById('form');
+const name = document.getElementById('name');
+const surname = document.getElementById('surname');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password1 = document.getElementById('password');
+const password2 = document.getElementById('password-repeat');
 
-// var inputList = document.getElementsByTagName("input")
-// console.log(inputList.length)
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    checkInput([name,surname,username, email, password1, password2]);
 
-// window.addEventListener('load',function(){
-
-
-//     let btnSubmit = this.document.getElementsByClassName("submit")
-//     btnSubmit.onclick=function(){
-//         let txtCardNumber=document.getElementsById('payment-card')
-//         let txtCardOwner = document.getElementById('payment-address')
-//         console.log("Included")
-//     }
-
-
-// })
-
-// window.onload = InvalidMsg(textbox);
-
-// function main(){
-//     let txtCard = document.getElementById("payment-card")
-
-//     txtCard.onchange = function() {
-//         let valid = false
-
-//         if (!valid) {
-//             txtCard.setCustomValidity ("Card is invalid or the number is wrong")
-//         }
-//         else{
-//             this.setCustomValidity('')
-//         }
-//     }
-// }
-
-function InvalidName(textbox) {
-
-    if (textbox.validity.patternMismatch) {
-        textbox.setCustomValidity('Name can only contain upper and lowercase letters. Try again');
-    }
-    else {
-        textbox.setCustomValidity('');
-    }
-    return true;
-}
-
-function InvalidSurname(textbox) {
-
-    if (textbox.validity.patternMismatch) {
-        textbox.setCustomValidity('Surname can only contain upper and lowercase letters. Try again!');
-    }
-    else {
-        textbox.setCustomValidity('');
-    }
-    return true;
-}
-
-function InvalidSurname(textbox) {
-
-    if (textbox.validity.patternMismatch) {
-        textbox.setCustomValidity('Surname can only contain upper and lowercase letters. Try again!');
-    }
-    else {
-        textbox.setCustomValidity('');
-    }
-    return true;
-}
-
-function InvalidDate(textbox) {
-    var date = this.getValue();
-    var now = new Date();
-    //Validate birth date some time before today's date and
-    //within 120 years
-    if (date == null {
-        textbox.setCustomValidity("Can't be empty");
-
+    if (!validateEmail(email.value.trim())) {
+        showerror(email, 'e-mail is not corrected');
     } else {
-        false;
+        showsuccess(email);
+    }
+    checkPassword(password1, password2);
+    checkInputLength(username, 5, 10);
+    checkInputLength(username, 5, 30);
+    checkInputLength(username, 5, 10);
+    checkInputLength(password1, 5, 12);
+
+});
+
+function showerror(input, message) {
+    const formControl = input.parentElement; //Work under main element (username under class=form-control)
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+function showsuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+
+}
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function checkInput(inputArray) {
+    inputArray.forEach(function(input) {
+        if (input.value.trim() === '') {
+            showerror(input, `Please complete ${getInputCase(input)}`);
+        } else {
+            showsuccess(input);
+        }
+    });
+}
+
+function getInputCase(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+
+}
+
+function checkPassword(password1, password2) {
+    if (password1.value !== password2.value) {
+        showerror(password2, 'Password is not match')
     }
 }
 
-// let cardValidatorError = document.querySelectorAll("#payment + span")
-// cardValidatorError.innerHTML = <i class = "fas fa-exclamation-circle"></i>
-//                                 'Invalid Card'
+function checkInputLength(input, min, max) {
+    if (input.value.length <= min) {
+        showerror(input, `${getInputCase(input)} username should be more than ${min} characters`)
+    } else if (input.value.length > max) {
+        showerror(input, `${getInputCase(input)} username should be less than ${max} characters`)
+    } else {
+        showsuccess(input);
+    }
 
-// const email = document.getElementById("email");
-
-// email.addEventListener("input", function (event) {
-//   if (email.validity.typeMismatch) {
-//     email.setCustomValidity("I am expecting an e-mail address!");
-//   } else {
-//     email.setCustomValidity("");
-//   }
-// });
+}
--- Exercise5/validation.js
+++ Exercise5/validation.js
@@ -1,9 +1,66 @@
-var myButton = document.getElementById("btnPopup");
-myButton.addEventListener("click" , function(){
-    /*
-        --Your Validation logic goes here--
-    */
+// var myButton = document.getElementById("btnPopup");
+// myButton.addEventListener("click" , function(){
+//     /*
+//         --Your Validation logic goes here--
+//     */
 
-    // You submit the Form when the Validation is over.
-    document.getElementById("frmUsers").submit();
-});
+//     // You submit the Form when the Validation is over.
+//     document.getElementById("frmUsers").submit();
+// });
+
+var formRegister = document.querySelector("#register-form")
+var password1 = document.querySelector("#password")
+var password2 = document.querySelector("#password-repeat")
+var birthdayInput = document.querySelector("#birthday")
+
+
+birthdayInput.addEventListener("input", () => {
+    let date = new Date(birthdayInput.value)
+    let currentDate = new Date()
+    let validDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDay(), 0, 0, 0, 0)
+
+    if (validDate > date) {
+        birthdayInput.setCustomValidity("You have to be over 16 to join the club :) We are sorry about this ask your parents if you still wish to enter!")
+        birthdayInput.reportValidity()
+    }else {
+        birthdayInput.setCustomValidity("")
+        birthdayInput.reportValidity()
+    }
+})
+
+
+
+
+password1.addEventListener("input", () => {
+    password2.setCustomValidity("")
+    password2.reportValidity()
+})
+
+password2.addEventListener("input", () => {
+    password2.setCustomValidity("")
+    password2.reportValidity()
+})
+
+formRegister.onsubmit = customSubmit
+
+function customSubmit(event) {
+    event.preventDefault()
+
+    let areSamePasswords = password1.value === password2.value
+    if (!areSamePasswords) {
+        password2.setCustomValidity("Passwords are not identical. Make sure you are typing the same password")
+        password2.reportValidity()
+        return
+    }else{
+        password2.setCustomValidity("")
+        password2.reportValidity()
+    }
+
+    Swal.fire(
+        'You registered',
+        'You have been added to the iOS club! You will be redirected in 3 seconds!',
+        'success'
+    )
+
+
+}
